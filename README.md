9a8fb963e49ec709f81cd483503863533d00aff7
mdsp <mdsp.personal@gmail.com>
2022-07-04 11:34:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+npm init -y  <!-- cria package.json -->
+npm install nodemon -D <!-- rodar  -->
+npm install express <!-- instala pacote express pelo npm -->
+npm install prisma -D
+npm install @prisma/client <!-- npx prisma init -->
+npx prisma generate
---
+++ index.js
@@ -0,0 +1,42 @@
+// config inicial/ instancia do express
+const express = require('express')
+const app = express()
+
+//config prisma
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
+
+// config p aceitar json
+app.use(express.json())
+
+
+app.get("/", (req,res) => {
+    return res.send("Hello World")
+})
+
+// rota post p receber dados
+app.post("/form", async (req,res) => {
+    const {nome, email, senha} = req.body
+
+    await prisma.usuario.create({
+        data: {
+            nome,
+            email,
+            senha
+        }
+    })
+    return res.status(201).send("Usuario criado com sucesso")
+})
+
+// listagem de usuarios
+app.get("/usuarios", async(req,res) => {
+    const usuarios = await prisma.usuario.findMany()
+
+    return res.status(200).send(usuarios)
+})
+
+// inicia servidor
+app.listen(3030,() => {
+    console.log("Server is running")
+});
